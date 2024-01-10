3fbef296945a789a744db37f4d397dcdda38f20c
Carina Silva <cass.carinasilva@gmail.com>
2024-01-10 16:42:32 -0300

    chore: alteracao na composição

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -1,34 +1,32 @@
 import React from "react";
 import style from "./style.module.css";
 import Button from "../Button";
-import classNames from 'classnames';
+
 interface AlertBarProps {
-  title: string
-  description?: string
+  title: string;
+  description?: string;
   IconClose: React.ElementType;
-  icon?: string
+  Icon?: React.ElementType;
 }
 
-const AlertBar = ({ title, description, IconClose, icon} : AlertBarProps) => {
-  const containerClassnames = classNames('material-icons');
-
+const AlertBar = ({ title, description, IconClose, Icon }: AlertBarProps) => {
   return (
     <div className={style.containerAlertBar}>
-        <div className={style.icons} >
+      <div className={style.icons}>
         <IconClose />
-        </div>
-        <div>
-        <i className={containerClassnames}>{icon}</i>
-        </div>
+      </div>
+      <div>
+        {Icon && <Icon />}
+      </div>
       <div className={style.contentAlertBar}>
         <h2 className={style.title}>{title}</h2>
         <p className={style.description}>{description}</p>
         <div className={style.containerButton}>
-            <Button className={style.button} label="Click here" size="s" />
+          <Button className={style.button} label="Click here" size="s" />
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AlertBar
+export default AlertBar;
