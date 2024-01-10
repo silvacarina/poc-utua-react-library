334b9192e96d983d0128147d67dd5bcc45481f69
Aline Cascaes <alinemcascaes@gmail.com>
2024-01-10 18:30:40 -0300

    alertbar

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import style from "./style.module.css";
 import Button from "../Button";
 
+interface IconProps {
+  variant?: string;
+}
 interface AlertBarProps {
   title: string;
   description?: string;
+  variant?: 'info' | 'check' | 'error' | 'warning';
   IconClose: React.ElementType;
-  IconNotification?: React.ElementType;
+  IconNotification?: React.ElementType<IconProps>;
 }
 
-const AlertBar = ({ title, description, IconClose, IconNotification: Icon }: AlertBarProps) => {
+const AlertBar = ({ title, description, IconClose, IconNotification: Icon, variant='info' }: AlertBarProps) => {
+  const iconVariantClass = Icon ? `icon-${variant.toLowerCase()}` : '';
+
   return (
     <div className={style.containerAlertBar}>
-      <div>
-        {Icon && <Icon />}
+      <div className={`${style.IconNotification} ${iconVariantClass}`}>
+      {Icon && <Icon variant={variant} />}
       </div>
       <div className={style.contentAlertBar}>
-        <h2 className={style.title}>{title}</h2>
+        <h3 className={style.title}>{title}</h3>
         <p className={style.description}>{description}</p>
         <div><Button label="Click here" size="white" variant="secondary" /></div>
       </div>
