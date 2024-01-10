ba3e46f9d18272d6c5cb060455f15e7ac5538ef1
Aline Cascaes <alinemcascaes@gmail.com>
2024-01-10 16:22:55 -0300

    Co-authored-by: Carina Silva <silvacarina@users.noreply.github.com>

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -1,24 +1,29 @@
+import React from "react";
 import style from "./style.module.css";
 import Button from "../Button";
-
+import classNames from 'classnames';
 interface AlertBarProps {
   title: string
   description?: string
-  iconinfo: string
-  iconclose: string
+  IconClose: React.ElementType;
+  icon?: string
 }
 
-const AlertBar = ({ title, description, iconinfo, iconclose } : AlertBarProps) => {
+const AlertBar = ({ title, description, IconClose, icon} : AlertBarProps) => {
+  const containerClassnames = classNames('material-icons');
+
   return (
     <div className={style.containerAlertBar}>
-        <div className={style.icons}>
-          <img src={iconinfo} alt="Icon 1" className={style.iconinfo} />
-          <img src={iconclose} alt="Icon 2" className={style.iconclose} />
+        <div className={style.icons} >
+        <IconClose />
+        </div>
+        <div>
+        <i className={containerClassnames}>{icon}</i>
         </div>
       <div className={style.contentAlertBar}>
         <h2 className={style.title}>{title}</h2>
         <p className={style.description}>{description}</p>
-        <div className={style.divbutton}>
+        <div className={style.containerButton}>
             <Button className={style.button} label="Click here" size="s" />
         </div>
         </div>
