283b0be6549d3a455f2a5ad3c3d1857f925b041c
Carina Silva <cass.carinasilva@gmail.com>
2024-01-10 19:05:46 -0300

    style: alteracao no css

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from 'classnames';
 import style from "./style.module.css";
 import Button from "../Button";
 
@@ -8,18 +9,19 @@ interface IconProps {
 interface AlertBarProps {
   title: string;
   description?: string;
-  variant?: 'info' | 'check' | 'error' | 'warning';
+  iconVariant?: 'info' | 'check' | 'error' | 'warning';
   IconClose: React.ElementType;
   IconNotification?: React.ElementType<IconProps>;
 }
 
-const AlertBar = ({ title, description, IconClose, IconNotification: Icon, variant='info' }: AlertBarProps) => {
-  const iconVariantClass = Icon ? `icon-${variant.toLowerCase()}` : '';
+const AlertBar = ({ title, description, IconClose, IconNotification: Icon, iconVariant='info' }: AlertBarProps) => {
 
   return (
-    <div className={style.containerAlertBar}>
-      <div className={`${style.IconNotification} ${iconVariantClass}`}>
-      {Icon && <Icon variant={variant} />}
+    <div className={classNames(style.containerAlertBar)}>
+      <div className={classNames(style.iconNotification, {
+        [style[iconVariant]]: Boolean(Icon)
+      })}>
+      {Icon && <Icon variant={iconVariant} />}
       </div>
       <div className={style.contentAlertBar}>
         <h3 className={style.title}>{title}</h3>
