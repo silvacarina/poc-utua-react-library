9e0a2618a6b4af37fa5e944d1166a465396fdb77
Carina Silva <cass.carinasilva@gmail.com>
2024-01-10 19:35:14 -0300

    style: estilização do componente

--- src/components/AlertBar/index.tsx
+++ src/components/AlertBar/index.tsx
@@ -3,21 +3,19 @@ import classNames from 'classnames';
 import style from "./style.module.css";
 import Button from "../Button";
 
-interface IconProps {
-  variant?: string;
-}
 interface AlertBarProps {
   title: string;
   description?: string;
   iconVariant?: 'info' | 'check' | 'error' | 'warning';
+  type: 'light' | 'dark'
   IconClose: React.ElementType;
-  IconNotification?: React.ElementType<IconProps>;
+  IconNotification?: React.ElementType;
 }
 
-const AlertBar = ({ title, description, IconClose, IconNotification: Icon, iconVariant='info' }: AlertBarProps) => {
+const AlertBar = ({ title, description, IconClose, IconNotification: Icon, iconVariant='info', type='light' }: AlertBarProps) => {
 
   return (
-    <div className={classNames(style.containerAlertBar)}>
+    <div className={classNames(style.containerAlertBar, style[`type-${type}`])}>
       <div className={classNames(style.iconNotification, {
         [style[iconVariant]]: Boolean(Icon)
       })}>
