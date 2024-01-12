6a551a3f976798f83b6aae4286cc9fdc78884667
Carina Silva <cass.carinasilva@gmail.com>
2024-01-12 10:37:18 -0300

    chore: add teste de tokens

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './styles/style.global.css'
+import './styles/style.tokens.css'
 
 export { AlertBar } from './components/AlertBar/AlertBar'
 export { Button } from './components/Button/Button'
